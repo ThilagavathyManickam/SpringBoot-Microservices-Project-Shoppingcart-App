9bccaea1850c89bde96b91b83e073e467e93e6ca
Thilagavathy M <89313155+ThilagavathyManickam@users.noreply.github.com>
2021-12-03 17:36:12 +0530

    Delete src directory

--- src/order-service/src/main/java com thilagalabs order service/repositories/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.thilagalabs.orderservice.repositories;
-
-import com.thilagalabs.orderservice.entities.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
